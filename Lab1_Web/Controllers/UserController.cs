d4bb33b16dfc63f80e912047d8b20f5c17213fab
sirfromfrance <107402489+sirfromfrance@users.noreply.github.com>
2023-11-22 21:15:48 +0200

    Added post method for user

--- Lab1_Web/Controllers/UserController.cs
+++ Lab1_Web/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Lab1_Web.Services;
+﻿using Common.Models;
+using Lab1_Web.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lab1_Web.Controllers;
@@ -24,4 +25,17 @@ public class UserController : Controller
         return View("AllUsers", users);
     }
 
+    [HttpGet]
+    public async Task<IActionResult> CreateUser()
+    {
+        return View("CreateUser");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateUser(UserCreateModel user)
+    {
+        await _service.CreateUser(user);
+
+        return RedirectToAction("GetAllUsers");
+    }
 }
